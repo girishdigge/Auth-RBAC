5e84cbb98676a52dbe6d8bc4ba424e60726ee51e
girishdigge <girishdigge@gmail.com>
2024-02-08 00:18:06 +0530

    login form created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { LoginButton } from '@/components/auth/login-button';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div>Project inception!</div>;
+  return (
+    <main className='flex h-full flex-col items-center justify-center'>
+      <div className='space-y-6 text-center'>
+        <h1 className='text-6xl font-semibold  drop-shadow-md'>🔐Auth</h1>
+        <p className=' text-lg'>A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button size={'lg'}>Sign in</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }

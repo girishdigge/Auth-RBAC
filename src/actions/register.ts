fa66aef8d612f757a404d455c04ba5d6d79ed9e2
girishdigge <girishdigge@gmail.com>
2024-02-08 13:29:51 +0530

    register page added

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,14 +1,7 @@
 'use server';
-import { LoginSchema } from '@/schemas';
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
-export const login = async (values: z.infer<typeof LoginSchema>) => {
-  const validatedFields = LoginSchema.safeParse(values);
-  if (!validatedFields.success) {
-    return { error: 'Invalid Fields!' };
-  }
-  return { success: 'Email sent!' };
-};
+
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {

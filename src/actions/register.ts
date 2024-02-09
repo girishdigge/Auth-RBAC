28d3df42bc3b324b7e237ba9f828d7da422e3f8f
girishdigge <girishdigge@gmail.com>
2024-02-09 17:30:30 +0530

    register complete

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,11 +1,28 @@
 'use server';
+import bcrypt from 'bcrypt';
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: 'Invalid Fields!' };
   }
-  return { success: 'Email sent!' };
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingEmail = await getUserByEmail(email);
+  if (existingEmail) {
+    return { error: 'Email already in use!' };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  //TODO: send verification token email!
+  return { success: 'User Created successfully!' };
 };

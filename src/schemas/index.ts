dccebe76eb690b44cc937016b1f269eca89616e0
girishdigge <girishdigge@gmail.com>
2024-02-12 14:05:05 +0530

    new password form added

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -24,3 +24,9 @@ export const ResetSchema = z.object({
     message: 'email is required',
   }),
 });
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: 'Minimum of 6 characters required',
+  }),
+});
